414f52a39cd693ff9774bbd3b4c9a7774b4687e8
AbuzarUsmani1994 <69624982+AbuzarUsmani1994@users.noreply.github.com>
2020-11-10 16:31:26 +0500

    Changes

--- FOS.Services/FOSDataModel.Designer.cs
+++ FOS.Services/FOSDataModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\KSB\KSB Complaint(18-Oct-2020)\KSB Complaint(18-Oct-2020)\KSB Complaint\FOS.Services\FOSDataModel.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\KSBLatestCode\FOS.Services\FOSDataModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
